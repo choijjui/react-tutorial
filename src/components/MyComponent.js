e910a2d5d3220bcb372dc4a6b142ec1430b0e8ae
Choi Junah <jachoi@wemb.co.kr>
2019-07-08 16:30:22 +0900

    react component state

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -14,6 +14,17 @@ class MyComponent extends Component {
         age : PropTypes.number.isRequired
     }
 
+    constructor(props) {
+        super(props);
+        /*this.state = {
+            number: 0
+        }*/
+    }
+
+    state = {
+        number: 0
+    }
+
     render() {
         return (
             <Fragment>
@@ -23,9 +34,20 @@ class MyComponent extends Component {
                 <div>
                     age is {this.props.age}
                 </div>
+                <div>
+                    숫자 : {this.state.number}
+                </div>
+                <button onClick={this.onBtnClick()}>더하기</button>
             </Fragment>
 
         )
+    }
+
+    onBtnClick() {
+        // 컴포넌트라 리렌더링하도록하는 트리거 역할
+        // this.forceUpdate()를 이용하여 강제로 리렌더링을 할 수 있지만, 매우 비효율적인 방법
+        // NOTE : 배열의 경우 객체 사본을 만들고, 그 사본에 값을 업데이트한 후 사본으로 값을 설정하는 방식
+        this.setState({number: this.state.number+1})
     }
 }
 
