1a26d34064af89edd2d6f97fa01d255543040973
junah <choijunah@gmail.com>
2019-07-14 21:14:24 +0900

    React Component Life Cycle

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
-import React from 'react';
-import './App.css';
+//import React, {Fragment} from 'react';
+//import './App.css';
 import MyComponent from "./components/chapter1_4/MyComponent";
 import EventPractice from "./components/chapter1_4/EventPractice";
 import Validation from "./components/chapter5/Validation";
 import IterationSample from "./components/chapter6/IterationSample";
+import LifeCycleSample from "./components/chapter7/LifeCycleSample";
 
-export default function App() {
-    return (
-        //<MyComponent age={28}></MyComponent>
-        //<EventPractice></EventPractice>
-        //<Validation></Validation>
-        <IterationSample></IterationSample>
-    );
+// export default function App() {
+//     return (
+//         //<MyComponent age={28}></MyComponent>
+//         //<EventPractice></EventPractice>
+//         //<Validation></Validation>
+//         //<IterationSample></IterationSample>
+//         //<LifeCycleSample></LifeCycleSample>
+//     );
+// }
+
+import React, {Component} from 'react';
+
+class App extends Component {
+    state = {
+        color : ''
+    }
+    render() {
+        return (
+            <div>
+                <button onClick={this.handleButtonClick}>발생</button>
+                <LifeCycleSample color={this.state.color}></LifeCycleSample>
+            </div>
+        );
+    }
+
+    handleButtonClick = ()=>{
+        this.setState({color: this.randomColor()})
+    }
+
+    randomColor() {
+        return "#" + Math.floor(Math.random() * 16777215).toString(16)
+    }
 }
+
+export default App;
